d9ad7889b0f6449c5fca6131bf1ce7238adc4dc2
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-12-09 16:13:28 -0600

    updating

--- HW14.R
+++ HW14.R
@@ -76,8 +76,59 @@ lrtest(model_full)
 lrtest(model_reduced)
 
 # these tests are comparing the full model against a null model with only an intercept term, and the reduced model against a null model with only an intercept term. 
-# model_full vs reduced shows that we added 3 degrees of freedom, 
+
+# model_full vs reduced shows that we added 3 degrees of freedom
+# log likelihood is showing how well the model fits the data, higher log likelihood = better fit 
+# so bigger number = better 
+# full model is better than the null hypothesis 
+
+# model_reduced vs null shows we added two degrees of freedom 
+#  the reduced model is also better than the null hypothesis 
+
+# both models have a chisq probability of improvement by about 45 x (ie these models are much better than guessing) 
+# both models have a similar increase in improvement
+
+# this is similar to backwards regression in that we are finding that hte interaction doesn't improve much 
 
 ###########################################
 # OBJECTIVE 2
 ###########################################
+
+# A 
+# ------------------------------------------
+
+# loading libraries 
+library(AICcmodavg)
+
+# filtering and subsetting data 
+tick_adults = ticks %>% 
+  filter(life_stage == "Adults")
+
+tick_nymphs = ticks %>% 
+  filter(life_stage == "Nymphs") 
+
+# candidate models 
+
+model1 = lm(questing_duration ~ life_stage*humidity_index, data = ticks)    # full model + interaction 
+model2 = lm(questing_duration~life_stage + humidity_index, data = ticks)    # full model w/out interaction 
+model3 = lm(questing_duration ~ humidity_index, data = tick_adults)         # model of adults 
+model4 = lm(questing_duration ~ humidity_index, data = tick_nymphs)         # model of nymphs 
+model5 = lm(questing_duration ~ 1, data = ticks)                            # null model 
+
+
+# AICc 
+
+model_list = list(model1, model2, model3, model4, model5) 
+aictab(cand.set = model_list, second.ord = TRUE)
+
+# B 
+# --------------------------------------------
+
+# AICc liked 4 the most, closely followed by model 3  
+# model 4 accounted for 74% of the cumulative weight of hte models' importance, followed by model 3 
+# Model 4 was the model which only looked at humidity index among nymphs, while model 3 was the closely related model that only looked at data from adults 
+# in other words, since AICc punishes pointless extra factors in your model, it's unsurprising htat it preferred hte simpler models 
+
+summary(model4)
+
+# Ecologically, we are seeing that ticks are willing to quest for victims for longer periods of time when the humidity index increases 
